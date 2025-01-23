1ea4bb01dbf483805512b37cd22d85a08769944a
BigZielina <242260@edu.p.lodz.pl>
2025-01-23 19:00:40 +0100

    rmse added yp bigdata_nb

--- gui.py
+++ gui.py
@@ -5,8 +5,10 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import streamlit as st
+from xgboost import XGBRegressor
+
 
 # Wczytanie danych
 data = pd.read_csv('Cleaned.csv')
@@ -27,12 +29,12 @@ y = data['price']  # Cena jako zmienna zależna
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Trenowanie modelu regresji liniowej
-model_lr = LinearRegression()
-model_lr.fit(X_train, y_train)
+model_xgb = XGBRegressor(random_state=42)
+model_xgb.fit(X_train, y_train)
 
-# Ocena modelu na zbiorze testowym
-y_pred = model_lr.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
+
+y_pred = model_xgb.predict(X_test)
+mse = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 # -----------------------------------------------
@@ -83,6 +85,6 @@ if st.button("Oblicz cenę"):
     input_data = input_data[X.columns]  # Zachowanie właściwej kolejności kolumn
 
     # Predykcja
-    prediction = model_lr.predict(input_data)
+    prediction = model_xgb.predict(input_data)
     st.subheader("Przewidywana cena:")
     st.write(f"Przewidywana cena: ${prediction[0]:,.2f}")
